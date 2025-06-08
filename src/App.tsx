2809cbec39d7f5f45c7157258f6d749ff406131f
Rishav Kumar <rishavjha462@gmail.com>
2025-06-08 21:47:34 +0530

    Make 'For Companies' list absolutely positioned at the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -462,7 +462,7 @@ function App() {
         <div className="grid grid-cols-1 md:grid-cols-2 w-full">
           {/* For Companies */}
           <div className="bg-black flex flex-col justify-between p-10 md:p-16 min-h-[400px] border-b-4 md:border-b-0 md:border-r-4 border-lime-400">
-            <div className="flex flex-col h-full">
+            <div className="flex flex-col flex-1 justify-between">
               <h3 className="text-6xl md:text-7xl font-extrabold text-white mb-20 -mt-2">For Companies</h3>
               <ul className="space-y-2 mt-auto pb-0">
                 <li className="flex items-center text-white text-lg md:text-xl font-normal leading-tight">
